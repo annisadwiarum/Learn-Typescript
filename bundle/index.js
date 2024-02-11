440b0ca10b5ae913a593fac80bbb13a984ded1af
annisadwiarum <annisadwiarum710@gmail.com>
2024-02-11 23:28:42 +0700

    type data, function, arrow function

--- bundle/index.js
+++ bundle/index.js
@@ -1,14 +1,48 @@
 "use strict";
-let firstName = "Annisa Dwi Arum";
-let username;
-let getMarried = false;
-let skill;
-let foods;
-username = 123;
-skill = ['programming', 12];
-foods = ['fried chicken', 'fried rice', 'baksoo', 10];
-console.log({ firstName });
-console.log({ username });
-console.log({ getMarried });
-console.log({ skill });
-console.log({ foods });
+// let firstName = "Annisa Dwi Arum";
+// let username: number | string;
+// let getMarried = false;
+// let skill: [string, number];
+// let foods: any[];
+// username = 123;
+// skill = ['programming', 12];
+// foods = ['fried chicken', 'fried rice', 'baksoo', 10];
+// console.log({firstName});
+// console.log({username});
+// console.log({getMarried});
+// console.log({skill});
+// console.log({foods});
+// type data custom
+// type PersonType = {
+//     name: string;
+//     age: number;
+//     gender: string;
+//     married: boolean;
+//     hutang?: number;
+// };
+// let MyFriend: PersonType;
+// let Me: PersonType;
+// MyFriend = {
+//     name: "XXX",
+//     age: 26,
+//     gender: "Male",
+//     married: false,
+//     hutang: 50000
+// }
+// Me = {
+//     name: "Anne",
+//     age: 24,
+//     gender: "female",
+//     married: false
+// }
+// console.log({MyFriend});
+// console.log({Me});
+// function
+function create() {
+    return 2;
+}
+console.log(create());
+// arrow function
+const createtest = () => "Hello World";
+const result = createtest();
+console.log(result);
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,62 @@
-let firstName = "Annisa Dwi Arum";
-let username: number | string;
-let getMarried = false;
-let skill: [string, number];
-let foods: any[];
+// let firstName = "Annisa Dwi Arum";
+// let username: number | string;
+// let getMarried = false;
+// let skill: [string, number];
+// let foods: any[];
 
-username = 123;
-skill = ['programming', 12];
-foods = ['fried chicken', 'fried rice', 'baksoo', 10];
+// username = 123;
+// skill = ['programming', 12];
+// foods = ['fried chicken', 'fried rice', 'baksoo', 10];
 
-console.log({firstName});
-console.log({username});
-console.log({getMarried});
-console.log({skill});
-console.log({foods});
+// console.log({firstName});
+// console.log({username});
+// console.log({getMarried});
+// console.log({skill});
+// console.log({foods});
+
+
+
+// type data custom
+
+// type PersonType = {
+//     name: string;
+//     age: number;
+//     gender: string;
+//     married: boolean;
+//     hutang?: number;
+// };
+
+// let MyFriend: PersonType;
+// let Me: PersonType;
+
+// MyFriend = {
+//     name: "XXX",
+//     age: 26,
+//     gender: "Male",
+//     married: false,
+//     hutang: 50000
+// }
+
+// Me = {
+//     name: "Anne",
+//     age: 24,
+//     gender: "female",
+//     married: false
+// }
+
+// console.log({MyFriend});
+// console.log({Me});
+
+
+// function
+function create(): number {
+    return 2;
+}
+
+console.log(create());
+
+// arrow function
+const createtest = (): string => "Hello World";
+const result = createtest();
+
+console.log(result);
