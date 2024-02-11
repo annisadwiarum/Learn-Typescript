231615c7e55fc0f2340359b28975597f2faa2a62
annisadwiarum <annisadwiarum710@gmail.com>
2024-02-11 22:04:28 +0700

    array

--- bundle/index.js
+++ bundle/index.js
@@ -2,7 +2,13 @@
 let firstName = "Annisa Dwi Arum";
 let username;
 let getMarried = false;
+let skill;
+let foods;
 username = 123;
+skill = ['programming', 12];
+foods = ['fried chicken', 'fried rice', 'baksoo', 10];
 console.log({ firstName });
 console.log({ username });
 console.log({ getMarried });
+console.log({ skill });
+console.log({ foods });
